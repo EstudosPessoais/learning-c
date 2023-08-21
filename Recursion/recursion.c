889843df2dda98993d639ad0fed11527bd0783d9
bielb1b2 <gabrielvt.souza@hotmail.com>
2023-08-21 00:48:54 -0300

    adding .gitignore and recursion sample. Removing output files.

---
+++ Recursion/recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int potencia(int base, int ep)
+{
+    if (ep == 1)
+    {
+        return base;
+    }
+    return base * potencia(base, ep - 1);
+}
+
+int main()
+{
+    int v = potencia(2, 4); // 16
+    printf("%d", v);
+    return 0;
+}
